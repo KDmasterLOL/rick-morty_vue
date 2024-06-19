e1e5ceb255f7baeef1dbf3039f215cd66fc63517
KDmasterLOL <ivantolmachevmaster@gmail.com>
2024-06-19 16:52:28 +0300

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test_task_vue/',
+  base: '/rick-morty_vue/',
   plugins: [
     vue(),
   ],
